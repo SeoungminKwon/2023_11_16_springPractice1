bc2655ec24bb7f26ea345974d87dc09d73c1eef8
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-11-16 01:52:33 +0900

    setting

---
+++ src/main/java/com/example/sb_prac1/SbPrac1Application.java
@@ -0,0 +1,13 @@
+package com.example.sb_prac1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPrac1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbPrac1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sb_prac1/SbPrac1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_prac1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbPrac1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
